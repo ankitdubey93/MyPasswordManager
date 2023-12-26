74c552d488017025f170d332a0668702bf0de07a
ankitdubey93 <ankit19933011@gmail.com>
2023-12-26 17:13:07 +0530

    created the password creation form and console logged it using javascript

---
+++ mpm.js
@@ -0,0 +1,8 @@
+function savePassword() {
+  const website = document.getElementById('website-platform').value;
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  console.log(website, username, password);
+
+}
