e70e828ea4a7485e6e62bd60a70aa703f478a7ff
ankitdubey93 <ankit19933011@gmail.com>
2023-12-28 10:08:54 +0530

    working password manager.with display table

--- mpm.js
+++ mpm.js
@@ -1,8 +1,49 @@
-function savePassword() {
-  const website = document.getElementById('website-platform').value;
-  const username = document.getElementById('username').value;
-  const password = document.getElementById('password').value;
+let formDataArray = [];
 
-  console.log(website, username, password);
 
+
+
+const savePassword = () => {
+  let website = document.getElementById('website').value;
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+
+  let formData = {
+    account: website,
+    id: username,
+    pass: password
+  };
+
+  console.log(formData);
+
+  formDataArray.push(formData);
+
+  console.log(formDataArray);
+
+  displayFormData();
+
+  document.getElementById('myForm').reset();
+
+}
+
+
+
+const displayFormData = () => {
+
+  let tbody = document.getElementById('formDataTable').getElementsByTagName('tbody')[0];
+
+  tbody.innerHTML = '';
+
+  formDataArray.forEach(function (data) {
+    let row = tbody.insertRow();
+    let c1 = row.insertCell(0);
+    let c2 = row.insertCell(1);
+    let c3 = row.insertCell(2);
+
+
+    c1.textContent = data.account;
+    c2.textContent = data.id;
+    c3.textContent = data.pass;
+
+  });
 }
